0fbd286f529ab3c62f4434dfcc00c2e38d114643
Harishmaheshkumar <122071687+Harishmaheshkumar@users.noreply.github.com>
2023-03-04 10:28:35 +0530

    Update seaborn.py

--- seaborn.py
+++ seaborn.py
@@ -11,4 +11,32 @@ df.dtypes
 sns.relplot(x="Views",y="Upvotes",data=df)
 
 sns.relplot(x="Views" , y="Upvotes" , hue="tag" , data=df)
+
+sns.catplot(x="education",y="avg_training_score",data=df)
+
+sns.catplot(x="education",y="avg_training_score",jitter=false,data=df2)
+
+sns.catplot(x="education",y="avg_training_score",hue="gender",data=df2)
+
+sns.catplot(x="education",y="avg_training_score",kind="swarm",data=df2)
+
+sns.catplot(x="education",y="avg_training_score",kind="box",data=df2)
+
+sns.catplot(x="education",y="avg_training_score",hue="is_promoted,kind="violin",data=df2)
+            
+sns.catplot(x="education",y="avg_training_score",hue="is_promoted,kind="violin",data=df2)
      
+sns.catplot(x="education",y="avg_training_score",hue="is_promoted,kind="violin",split=true,data=df2)
+
+sns.catplot(x="education",y="avg_training_score",hue="is_promoted,kind="bar",data=df2)
+          
+sns.catplot(x="education",y="avg_training_score",hue="is_promoted,kind="point",data=df2)
+           
+
+
+            
+ 
+
+
+     
+
